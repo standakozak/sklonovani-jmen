050245538c0899f70b666693342fc05e7df3889d
standakozak <standa23k@seznam.cz>
2021-08-22 22:22:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'sklonovani_jmen',
   packages = ['sklonovani_jmen'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='MIT',
   description = 'Module for using the API of www.sklonovani-jmen.cz in Python',
   author = 'Stanislav Kozák',
   author_email = 'kozakstanda23@gmail.com',
   url = 'https://github.com/standakozak/sklonovani_jmen',
-  download_url = 'https://github.com/standakozak/sklonovani_jmen/archive/refs/tags/v_011.tar.gz',
+  download_url = 'https://github.com/standakozak/sklonovani_jmen/archive/refs/tags/v_012.tar.gz',
   keywords = ['sklonovani-jmen', 'Czech grammar', 'API'],
   install_requires=[
           'requests'
